3c377fafcc071b46b061cf05e271b30e77adc3bc
yue.fan <yue.fan@thoughtworks.com>
2020-11-22 05:29:57 -0500

    async mock

--- src/async.mock/__test__/user.test.js
+++ src/async.mock/__test__/user.test.js
@@ -4,11 +4,13 @@ jest.mock("../verify");
 jest.mock("axios");
 
 describe("register", () => {
-  test("should post user when validated", () => {
+  test("should post user when validated", async () => {
     // TODO 19: add test here
+    await expect(register()).resolves.toEqual({});
   });
 
-  test("should reject with Error when username is invalid", () => {
+  test("should reject with Error when username is invalid", async () => {
     // TODO 20: add test here
+    await expect(register()).rejects.toThrow("wrong username or password");
   });
 });
--- src/async/__mocks__/axios.js
+++ src/async/__mocks__/axios.js
@@ -1,3 +1,4 @@
 export default {
   get: jest.fn(() => Promise.resolve({ data: { name: "yue" } })),
+  post: jest.fn(() => Promise.resolve({ data: {} })),
 };
